94daa0c5dae8211ae37c0c40269f3d58095a8dd8
Gabriel Buzukashvili <gabriel.bazak@gmail.com>
2023-06-30 16:46:31 +0300

    finish signup view.email confirmation works

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -34,13 +35,13 @@ def signup(request):
         confirmation_code = str(uuid.uuid4())
         email_subject = 'Your confirmation code'
         message = f'Ваш код: {confirmation_code}'
+        to_email = request.data['email']
         try:
             send_mail(
                 subject=email_subject,
                 message=message,
-                from_email=None,
-                recipient_list=(request.POST.get('email'),),
-                fail_silently=False,
+                from_email='from@example.com',
+                recipient_list=(to_email,),
             )
         except Exception:
             return Response(status=status.HTTP_400_BAD_REQUEST)
